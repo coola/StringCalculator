62ace74f798601eb499a37e7595b19fba29c3c50
Michał Kuliński <michal.kulinski@accenture.com>
2014-12-19 16:34:02 +0100

    Initial commit

---
+++ src/stringCalculator.js
@@ -0,0 +1,20 @@
+function StringCalculator() {
+
+    this.add = function (stringArgument) {
+
+        var numbers = stringArgument.split(",");
+
+        var result = 0;
+        for(var i = 0 ; i< numbers.length; i++){
+          result += parseNumber(numbers[i]);
+        }
+
+        return result;
+
+    };
+}
+
+function parseNumber(argument) {
+    if (argument != "") return parseInt(argument);
+    return 0;
+}
---
+++ test/stringCalculator.test.js
@@ -0,0 +1,69 @@
+describe("Karma", function(){
+    it("should work", function(){
+        expect(true).toBe(true);
+    });
+});
+
+describe("StringCalculator", function(){
+    it("should add exist", function(){
+        var stringCalculator = new StringCalculator();
+        expect(stringCalculator.add("")).toBe(0);
+    });
+});
+
+describe("StringCalculator", function(){
+    it("should add exist", function(){
+        var stringCalculator = new StringCalculator();
+        expect(stringCalculator.add("")).toBe(0);
+    });
+
+    it("should add 1", function(){
+        var stringCalculator = new StringCalculator();
+        expect(stringCalculator.add("1")).toBe(1);
+    });
+
+    it("should add 2", function(){
+        var stringCalculator = new StringCalculator();
+        expect(stringCalculator.add("2")).toBe(2);
+    });
+
+    it("should add 1000", function(){
+        var stringCalculator = new StringCalculator();
+        expect(stringCalculator.add("1000")).toBe(1000);
+    });
+
+    /*it("should add 1", function(){
+        var stringCalculator = new StringCalculator();
+        expect(stringCalculator.add("1,2")).toBe(3);
+    });*/
+
+    it("two empty arguments", function(){
+        var stringCalculator = new StringCalculator();
+        expect(stringCalculator.add(",")).toBe(0);
+    });
+
+    it("two zero arguments", function(){
+        var stringCalculator = new StringCalculator();
+        expect(stringCalculator.add("0,0")).toBe(0);
+    });
+
+    it("first 0, second argument 1", function(){
+        var stringCalculator = new StringCalculator();
+        expect(stringCalculator.add(",1")).toBe(1);
+    });
+
+    it("first 1, second argument 1", function(){
+        var stringCalculator = new StringCalculator();
+        expect(stringCalculator.add("1,1")).toBe(2);
+    });
+
+    it("first 1, second argument 1, third 1", function(){
+        var stringCalculator = new StringCalculator();
+        expect(stringCalculator.add("1,1, 1")).toBe(3);
+    });
+
+    it("more complex example", function(){
+        var stringCalculator = new StringCalculator();
+        expect(stringCalculator.add("333,333, 333")).toBe(999);
+    });
+});
